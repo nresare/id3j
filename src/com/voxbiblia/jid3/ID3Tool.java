099d92642d5618e81b60908c1f50af80eefb697f
noa <noa@3beca68c-c521-0410-ae18-dd2a0ae60fc9>
2008-12-26 17:46:45 +0000

    marked all source files with the GPLv3 recommended file header
    git-svn-id: file:///home/noa/slask/svnroot/jid3/trunk@1740 3beca68c-c521-0410-ae18-dd2a0ae60fc9

--- src/com/voxbiblia/jid3/ID3Tool.java
+++ src/com/voxbiblia/jid3/ID3Tool.java
@@ -1,4 +1,25 @@
+/*
+    jid3 - a library that generates ID3v2 tags
+    Copyright (C) 2008  Noa Resare (noa@voxbiblia.com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Project web page: http://fs.voxbiblia.com/jid3/
+ */
 package com.voxbiblia.jid3;
+
+import java.io.File;
 
 /**
  * A tool class used to serialize audio metadata for inclusion into an
@@ -18,6 +39,31 @@ public class ID3Tool
     {
         ID3Serializer s = new ID3Serializer();
         return s.serialize(updatedValues, existing);
+    }
+
+    /**
+     * Converts the given tag instance to an ID3v2.3 byte stream
+     * and adds it to the target, replacing any existing tag.
+     *
+     * @param tag the tag to write
+     * @param target the file to write it to
+     */
+    public static void write(File target, ID3Tag tag)
+    {
+        throw new Error("not implemented");
+    }
+
+    /**
+     * Updates the ID3 tag in target with new values from tag. If a property
+     * in tag is not set, the corresponding frame is copied from the previous
+     * tag.
+     *
+     * @param target the file to update the ID3 tag in
+     * @param tag an ID3Tag instance that contains updated properties
+     */
+    public static void update(File target, ID3Tag tag)
+    {
+        throw new Error("not implemented");
     }
 
     /**
--- test/src/com/voxbiblia/jid3/CRC32CompensatorTest.java
+++ test/src/com/voxbiblia/jid3/CRC32CompensatorTest.java
@@ -1,3 +1,22 @@
+/*
+    jid3 - a library that generates ID3v2 tags
+    Copyright (C) 2008  Noa Resare (noa@voxbiblia.com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Project web page: http://fs.voxbiblia.com/jid3/
+ */
 package com.voxbiblia.jid3;
 
 import junit.framework.AssertionFailedError;
